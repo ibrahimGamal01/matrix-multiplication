10437f774880db3216d889daf72df50321c81019
Ibrahim Gamal <116229968+ibrahimGamal01@users.noreply.github.com>
2023-02-22 05:02:26 +0200

    update and advance

--- matrix-multiplication.py
+++ matrix-multiplication.py
@@ -1,16 +1,36 @@
+import numpy as np  # I want to check my solution with numpy
 import random
-r1 = int(input("Enter mat1 row size: "))
-c1 = int(input("Enter mat1 col size: "))
-# r2 = int(input("Enter mat2 row size: "))
-# c2 = int(input("Enter mat2 col size: "))
+r1 = int(input("Enter matrix 1 number of rows : "))
+c1 = int(input("Enter matrix 1 number of cols : "))
+r2 = int(input("Enter matrix 2 number of rows : "))
+c2 = int(input("Enter matrix 2 number of cols : "))
 
+# initialize the matrix of zero values
 mat1 = [[0 for j in range(c1)] for i in range(r1)]
+mat2 = [[0 for j in range(c2)] for i in range(r2)]
 
+# Random Values Insertion
+# isert random values in matrix 1
 for i in range(r1):
     for j in range(c1):
-        mat1[i][j] = i+ j + random.randint(3,9)
+        mat1[i][j] = i + j + random.randint(3, 19)
+
+# insert random values in matrix 2
+for i in range(r2):
+    for j in range(c2):
+        mat2[i][j] = i + j + random.randint(3, 9)
+
+# print matr
 
 for i in range(r1):
     for j in range(c1):
         print(mat1[i][j], end=' ')
     print()  # add a newline after each row
+
+print("*******************************************")  # print
+
+for i in range(r2):
+    for j in range(c2):
+        print(mat2[i][j], end=' ')
+    print()  # add a newline after each row
+

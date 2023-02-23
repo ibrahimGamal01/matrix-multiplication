014ecfa6b0a048ed815a276f2be6d1b4fa61b58a
Ibrahim Gamal <116229968+ibrahimGamal01@users.noreply.github.com>
2023-02-23 03:56:40 +0200

    enhance

--- matrix-multiplication.py
+++ matrix-multiplication.py
@@ -5,10 +5,18 @@ c1 = int(input("Enter matrix 1 number of cols : "))
 r2 = int(input("Enter matrix 2 number of rows : "))
 c2 = int(input("Enter matrix 2 number of cols : "))
 
+
+def print_matrix(matrix):
+    for row in matrix:
+        for val in row:
+            print(val, end=" ")
+        print()
+
+
 if c1 == r2:
     # initialize the matrix of zero values
-    mat1 = [[random.randint(1,20) for j in range(c1)] for i in range(r1)]
-    mat2 = [[random.randint(1,20) for j in range(c2)] for i in range(r2)]
+    mat1 = [[random.randint(1, 20) for j in range(c1)] for i in range(r1)]
+    mat2 = [[random.randint(1, 20) for j in range(c2)] for i in range(r2)]
     ansMat = [[0 for j in range(c2)] for i in range(r1)]
 
     # Random Values Insertion
@@ -24,17 +32,16 @@ if c1 == r2:
 
     # print matr
 
-    for i in range(r1):
-        for j in range(c1):
-            print(mat1[i][j], end=' ')
-        print()  # add a newline after each row
+    print_matrix(mat1)
 
     print("*******************************************")  # print
 
-    for i in range(r2):
-        for j in range(c2):
-            print(mat2[i][j], end=' ')
-        print()  # add a newline after each row
+    # for i in range(r2):
+    #     for j in range(c2):
+    #         print(mat2[i][j], end=' ')
+    #     print()  # add a newline after each row
+    
+    print_matrix(mat2)
 
     print("============================================")  # print
 
@@ -43,10 +50,7 @@ if c1 == r2:
             for k in range(r2):
                 ansMat[i][j] += mat1[i][k] * mat2[k][j]
 
-    for i in range(r1):
-        for j in range(c2):
-            print(ansMat[i][j], end=' ')
-        print()
+    print_matrix(ansMat)
 
 else:  # c1 should = r2
     print("First matrix must have the same number of columns as the second matrix has rows c1 should = r2 ")

ed1c302bd849ffaf3416a603b70521583d397333
Ibrahim Gamal <116229968+ibrahimGamal01@users.noreply.github.com>
2023-02-23 03:31:28 +0200

    complete the code

--- matrix-multiplication.py
+++ matrix-multiplication.py
@@ -8,6 +8,7 @@ c2 = int(input("Enter matrix 2 number of cols : "))
 # initialize the matrix of zero values
 mat1 = [[0 for j in range(c1)] for i in range(r1)]
 mat2 = [[0 for j in range(c2)] for i in range(r2)]
+ansMat = [[0 for j in range(c2)] for i in range(r2)]
 
 # Random Values Insertion
 # isert random values in matrix 1
@@ -33,3 +34,15 @@ for i in range(r2):
     for j in range(c2):
         print(mat2[i][j], end=' ')
     print()  # add a newline after each row
+
+print("============================================")  # print
+
+for i in range(r1):
+    for j in range(c2):
+        for k in range(r2):
+            ansMat[i][j] += mat1[i][k] * mat2[k][j]
+
+for i in range(r1):
+    for j in range(c2):
+        print(ansMat[i][j], end=' ')
+    print()
